1acd06d5655fbd8ffafe806c11cef06cabd1a58a
akimitsu <akimitsu@akimitsu.top>
2022-11-07 17:28:03 +0800

    feat(init): 初始化后端项目，搭建基本结构

---
+++ relax/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RelaxConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "relax"
---
+++ soul/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SoulConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "soul"

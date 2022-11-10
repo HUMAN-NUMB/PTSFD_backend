f07b65133e15ac3dc854f974c3357f7d317fa5af
akimitsu <akimitsu@akimitsu.top>
2022-11-10 11:31:34 +0800

    fix(secert): 修复密钥文件

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
-import secert
+from . import secert
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
